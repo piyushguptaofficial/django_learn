5d2cee4891bd4cd257c1e8eb2e68f9026f3f6417
piyushguptaofficial <100441111+piyushguptaofficial@users.noreply.github.com>
2025-04-30 01:36:33 +0000

    Add mini project files for Phase 1

---
+++ django_learn/core/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductForm
+
+def home(request):
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
+
